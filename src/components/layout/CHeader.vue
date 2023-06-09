d5aae73ff1a8b75038f020294e75d0155b45b738
RadinNedkov <62220917+RadinNedkov@users.noreply.github.com>
2023-06-09 13:49:28 +0300

    small ui changes

--- src/components/layout/CHeader.vue
+++ src/components/layout/CHeader.vue
@@ -7,14 +7,14 @@ const mobileMenuOpen = ref(true)
   <header class="bg-white">
     <nav class="mx-auto flex items-center justify-between p-6 lg:px-8" aria-label="Global">
       <div class="flex lg:flex-1">
-        <a href="#" class="-m-1.5 p-1.5">
-          <span class="sr-only">Credit Management System</span>
-          <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-        </a>
+        <router-link to="/" class="-m-1.5 p-1.5 flex items-center">
+          <img class="h-8 w-auto mr-1" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Credit Management System" />
+          <span class="font-medium">Credit Management System</span>
+        </router-link>
       </div>
       <div class="flex lg:hidden">
-        <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
-          <span>menu</span>
+        <button type="button" class="-m-2.5 font-semibold inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
+          <span>-menu-</span>
         </button>
       </div>
       <div class="hidden lg:flex lg:gap-x-12">
@@ -30,20 +30,20 @@ const mobileMenuOpen = ref(true)
       <div class="fixed inset-0 z-10" />
       <div class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
         <div class="flex items-center justify-between">
-          <a href="#" class="-m-1.5 p-1.5">
+          <router-link to="/" class="-m-1.5 p-1.5">
             <span class="sr-only">Credit Management System</span>
             <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-          </a>
-          <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
-            <span>close</span>
+          </router-link>
+          <button type="button" class="-m-2.5 font-semibold rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
+            <span>-close-</span>
           </button>
         </div>
         <div class="mt-6 flow-root">
           <div class="-my-6 divide-y divide-gray-500/10">
             <div class="space-y-2 py-6">
-              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Credits</a>
-              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">New Credit</a>
-              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Make Payment</a>
+              <router-link to="/" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Credits</router-link>
+              <router-link to="/new-credit" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">New Credit</router-link>
+              <router-link to="/make-payment" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Make Payment</router-link>
             </div>
             <div class="py-6">
               <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</a>
